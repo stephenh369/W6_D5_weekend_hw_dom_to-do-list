036edc84421d24a42e6f3374692106d9b95de172
Stephen <stephen_h369@live.co.uk>
2020-07-24 19:47:42 +0100

    added handleFormSubmit function, minor html id and value changes

--- app.js
+++ app.js
@@ -7,3 +7,23 @@ document.addEventListener('DOMContentLoaded', () => {
     const deleteAll = document.querySelector('#delete-all');
     deleteAll.addEventListener('click', handleDeleteAllClick);
 });
+
+const handleFormSubmit = function(e) {
+    e.preventDefault();
+    console.log(this);
+    
+    const task = this.task.value;
+    const type = this.type.value;
+    const priority = this.priority.value;
+    const list = document.querySelector('#to-do-list');
+    const listItem = document.createElement('li');
+
+    listItem.textContent = `Task: ${task} Type: ${type} Priority: ${priority}`;
+    list.appendChild(listItem);
+
+    this.reset();
+};
+
+const handleDeleteAllClick = function(e) {
+
+}
