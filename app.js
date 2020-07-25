57516953ecdc5c20c5ddb3353bfe39d68027e6b8
Stephen <stephen_h369@live.co.uk>
2020-07-25 20:22:01 +0100

    task delete buttons now function

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const deleteAll = document.querySelector('#delete-all');
     deleteAll.addEventListener('click', handleDeleteAllClick);
+
 });
 
 const handleFormSubmit = function(e) {
@@ -16,20 +17,27 @@ const handleFormSubmit = function(e) {
     const type = this.type.value;
     const priority = this.priority.value;
     const list = document.querySelector('#to-do-list');
-    const listItem = document.createElement('li');
-    const deleteItem = document.createElement('button');
+    let listItem = document.createElement('li');
+    let deleteItem = document.createElement('button');
 
     listItem.textContent = `Task: ${task} Type: ${type} Priority: ${priority}`;
     deleteItem.innerHTML = "Delete";
     list.appendChild(listItem);
+    listItem.classList.add('list-item');
     listItem.appendChild(deleteItem);
-    deleteItem.setAttribute('id', 'delete-item');
-
+    deleteItem.classList.add('delete-item');
 
     this.reset();
+
+    list.addEventListener('click', handleDeleteItemClick);
 };
 
 const handleDeleteAllClick = function() {
     const list = document.querySelector('#to-do-list');
     list.innerHTML = '';
-}
+};
+
+const handleDeleteItemClick = function(e) {
+    const item = e.target;
+    item.parentNode.remove();
+};
